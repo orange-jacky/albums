243499abbf7bd5d96909ac7701e2ba73eccbe145
orangejackylee <orange.jackylee@gmail.com>
2017-10-11 10:00:12 +0800

    更新CORS

--- util/gin_use_middle.go
+++ util/gin_use_middle.go
@@ -7,8 +7,9 @@ import (
 func CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		resp := c.Writer
+		resp.Header().Set("Access-Control-Allow-Methods", "GET, POST")
 		resp.Header().Set("Access-Control-Allow-Origin", "*")
 		resp.Header().Set("Access-Control-Allow-Credentials", "true")
-		resp.Header().Set("Access-Control-Allow-Headers", "Content-Type,Content-Encoding,Cache-Control,Content-Length,Accept-Encoding ")
+		resp.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Encoding, Cache-Control, Content-Length, Accept-Encoding, Authorization, Origin, X-Requested-With, Accept")
 	}
 }
