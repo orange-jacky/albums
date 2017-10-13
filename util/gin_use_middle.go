4affa35429e4b4dff336b41ca62fb6b4c85a9418
orangejackylee <orange.jackylee@gmail.com>
2017-10-13 09:46:07 +0800

    增加CORS对options请求的支持

--- util/gin_use_middle.go
+++ util/gin_use_middle.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func CORS() gin.HandlerFunc {
@@ -11,5 +12,10 @@ func CORS() gin.HandlerFunc {
 		resp.Header().Set("Access-Control-Allow-Origin", "*")
 		resp.Header().Set("Access-Control-Allow-Credentials", "true")
 		resp.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Encoding, Cache-Control, Content-Length, Accept-Encoding, Authorization, Origin, X-Requested-With, Accept")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusOK)
+			return
+		}
 	}
 }
